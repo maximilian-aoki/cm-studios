a2c682fe9f0f27d3e5c91dd95aabc1b2f375afed
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2024-04-05 15:06:54 -0400

    design full home listing ui

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const data = [
   {
     id: 1,
     hash: 'c4ca4238a0b923820dcc509a6f75849b',
-    address: '1927 Davenport',
+    address: '1927 Davenport Rd',
     category: 'Residential',
     photoPaths: ['1', '2', '3', '4', '5'],
     thumbnailPath: ['1'],
@@ -10,7 +10,7 @@ export const data = [
   {
     id: 2,
     hash: 'c81e728d9d4c2f636f067f89cc14862c',
-    address: '1927 Davenport',
+    address: '1927 Davenport Rd',
     category: 'Commercial',
     photoPaths: ['6', '7', '8', '9', '10'],
     thumbnailPath: ['6'],
--- src/home-page/HomePage.jsx
+++ src/home-page/HomePage.jsx
@@ -7,14 +7,30 @@ export default function HomePage({ data }) {
   return (
     <div className={styles.homeContainer}>
       <h2>Listings</h2>
-      <div className={styles.listingsFrame}></div>
-      {/* {data.map((listing) => {
-        return (
-          <Link key={listing.id} to={`/listings/${listing.hash}`}>
-            {listing.id}
-          </Link>
-        );
-      })} */}
+      <div className={styles.listingsFrameFormatter}>
+        <div className={styles.listingsFrame}>
+          {data.map((listing) => {
+            return (
+              <Link
+                key={listing.id}
+                to={`/listings/${listing.hash}`}
+                className={styles.listingCard}
+              >
+                <div className={styles.image}>
+                  <img
+                    src="https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="real estate"
+                  />
+                </div>
+                <div className={styles.listingInfo}>
+                  <p className={styles.listingAddress}>{listing.address}</p>
+                  <p className={styles.listingCategory}>{listing.category}</p>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
